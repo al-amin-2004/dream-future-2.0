d80b9181d8ea8c4e47bbd423bc0e0718d2b4d585
Al amin <alaminmridha2004@gmail.com>
2026-02-18 00:36:00 +0600

    Header create with responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+import Header from "./_components/shared/Header";
+
 export default function Home() {
   return (
-    <div
+    <>
+      <div
         className="absolute w-full h-240 -z-50 top-0"
         style={{
           backgroundImage: `radial-gradient(circle at 85% 30%, rgba(255, 191, 0, 0.08) 0, transparent 40%)`,
         }}
       />
+      <Header />
+    </>
   );
 }
