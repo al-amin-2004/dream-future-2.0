09e948be0a2665bb5af12940c9b293c4e5ff2c0f
Al amin <alaminmridha2004@gmail.com>
2026-02-20 01:18:39 +0600

    Add resend dependency and implement sendVerificationEmail helper function

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,25 @@
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendVerificationEmail = async (email: string, code: string) => {
+  try {
+    await resend.emails.send({
+      from: "onboarding@resend.dev",
+      to: email,
+      subject: "OTP for Dream-Future",
+      html: `
+        <div style="font-family: sans-serif;">
+          <h2>Your OTP Code</h2>
+          <p style="font-size: 20px; font-weight: bold;">${code}</p>
+          <p>This code will expire in 1 minute.</p>
+        </div>
+      `,
+    });
+
+    return { success: true, message: "Verification email send successfully" };
+  } catch (error) {
+    console.error("Error sending verification email", error);
+    return { success: false, message: "Faild to send verification email" };
+  }
+};
