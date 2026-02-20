16bf97ab7f22380e8b35017d488f0d290cb4c1fb
Al amin <alaminmridha2004@gmail.com>
2026-02-20 10:07:39 +0600

    Add bcryptjs dependency and implement user registration with unique username generation and email verification

---
+++ src/app/api/auth/sign-up/route.ts
@@ -0,0 +1,75 @@
+import { generateUniqueUsername } from "@/helpers/generateUsername";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import ValidationModel from "@/models/OTPValidation";
+import UserModel from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+  try {
+    const { firstName, lastName, email, password } = await request.json();
+
+    const username = await generateUniqueUsername(firstName, lastName);
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const verifyCode = Math.floor(100000 + Math.random() * 900000).toString();
+    const hashedCode = await bcrypt.hash(verifyCode, 10);
+    const expiryDate = new Date();
+    expiryDate.setMinutes(expiryDate.getMinutes() + 2);
+
+    const existingEmail = await UserModel.findOne({ email });
+
+    if (existingEmail) {
+      if (existingEmail.isVerifiedEmail) {
+        return Response.json(
+          { success: false, message: "User already exist with this email." },
+          { status: 400 },
+        );
+      } else {
+        existingEmail.password = hashedPassword;
+        await existingEmail.save();
+        await ValidationModel.findOneAndUpdate(
+          { userId: existingEmail._id },
+          {
+            verificationCode: hashedCode,
+            expiresAt: expiryDate,
+          },
+          { upsert: true, new: true },
+        );
+      }
+    } else {
+      const createNewUser = await UserModel.create({
+        firstName,
+        lastName,
+        username,
+        email,
+        password: hashedPassword,
+      });
+
+      await ValidationModel.create({
+        userId: createNewUser._id,
+        verificationCode: hashedCode,
+        expiresAt: expiryDate,
+      });
+    }
+
+    // Send Verify Email
+    const emailRespons = await sendVerificationEmail(email, verifyCode);
+
+    if (!emailRespons.success) {
+      return Response.json(
+        { success: false, message: emailRespons.message },
+        { status: 500 },
+      );
+    }
+
+    return Response.json(
+      { success: true, message: "Sending an OTP. Please verify your email." },
+      { status: 201 },
+    );
+  } catch (error) {
+    console.error("Error registering user!", error);
+    return Response.json(
+      { success: false, message: "Error registering user!" },
+      { status: 500 },
+    );
+  }
+}
---
+++ src/helpers/generateUsername.ts
@@ -0,0 +1,31 @@
+import UserModel from "@/models/User";
+
+const createBaseUsername = (firstName: string, lastName?: string) => {
+  const clean = (str: string) => str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+
+  return clean(firstName) + (lastName ? clean(lastName) : "");
+};
+
+export const generateUniqueUsername = async (
+  firstName: string,
+  lastName?: string,
+): Promise<string> => {
+  const base = createBaseUsername(firstName, lastName);
+
+  let username: string = "";
+  let exist:boolean = true;
+
+  while (exist) {
+    const randomNumber = Math.floor(1000 + Math.random() * 9000);
+
+    username = `${base}@${randomNumber}`;
+
+    const existUser = await UserModel.find({ username });
+
+    if (!existUser) {
+      exist = false;
+    }
+  }
+
+  return username;
+};
